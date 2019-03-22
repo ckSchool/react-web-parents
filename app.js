9879551f7793edd81aad661dcc5a6bba57a1ec7b
Simon <simonmarkholland@gmail.com>
2019-03-22 11:18:16 +0700

    Update app.js
    Add listener to start server

--- app.js
+++ app.js
@@ -8,3 +8,11 @@ var express = require("express"),
     LocalStrategy = require("passport-local"),
     methodOverride = require("method-override"),
     User = require("./models/user");
+var listener = app.listen(process.env.PORT, process.env.IP, function(){
+    var address = listener.address().address;
+    var port = listener.address().port;
+  
+    console.log('React App server listening on : ' + address + ':' + port);
+    console.log('Time : ' + time);
+    console.log('Database in use :  \n' + databaseurl);
+});
