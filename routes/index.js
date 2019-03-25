58393e4f20cc5e8c4c9c01ff17cd6ab4542c7840
Simon <simonmarkholland@gmail.com>
2019-03-25 17:37:06 +0700

    Remove unused data (moved to student model)

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,16 @@ var router = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
 var middleware = require("../middleware");
-
-text = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias blanditiis quod sit fugiat placeat eaque aspernatur est qui. Vel, ipsa?"
+var students = require("../models/students");
+var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+date = new Date().toLocaleString('en-GB', options);
 
 router.get('/', function(req, res){
     res.render('index');
 });
 
 router.get('/home', middleware.isLoggedIn, function(req, res){
-    var sections = [
-        {title: "Heading 1", image: "imgs/teach.png", paragraph: text},
-        {title: "Heading 2", image: "imgs/apples.png", paragraph: text},
-        {title: "Heading 3", image: "imgs/glasses.png", paragraph: text}
-    ]
-
-    res.render('home', {articles: sections});
+    res.render('home', {students: students, date:date});
 });
 
 module.exports = router;
