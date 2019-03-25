d9c400efdcf5c339f1afc5aa49ae93310ed79e93
Simon <simonmarkholland@gmail.com>
2019-03-25 17:35:13 +0700

    Create basic student model

---
+++ models/students.js
@@ -0,0 +1,7 @@
+var students = [
+    {name: "Tommy Sinclair", picture: "imgs/teach.png", class: "SD 1", atSchool: "True"},
+    {name: "Clair Hazeldown", picture: "imgs/apples.png", class: "SMP 3", atSchool: "False"},
+    {name: "Trevor Noah", picture: "imgs/glasses.png", class: "SMA 2", atSchool: "True"}
+]
+
+module.exports = students;
